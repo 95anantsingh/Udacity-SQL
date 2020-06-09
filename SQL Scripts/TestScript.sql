1e52cc8ff91741a8c18f616a7cb2171737cee5ad
Anant <95anantsingh@gmail.com>
2020-06-09 09:14:11 +0530

    gitignore update

--- SQL Scripts/TestScript.sql
+++ SQL Scripts/TestScript.sql
@@ -1,2 +1,7 @@
 SELECT *
-	FROM demo.orders
+FROM demo.web_events
+WHERE channel IN ('organic', 'adwords')
+  AND occurred_at >= '2016-01-01'
+  AND occurred_at <= '2016-12-31'
+ORDER BY occurred_at DESC;
+  
